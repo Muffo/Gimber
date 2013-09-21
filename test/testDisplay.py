0503134df5fadd8b00e44384b4ceabab43284c45
Muffo <muffo87@gmail.com>
2013-09-21 19:36:43 +0100

    Tests for loadImage
    Added: test for the loadImage function of the Display

--- test/testDisplay.py
+++ test/testDisplay.py
@@ -10,6 +10,7 @@
 #-------------------------------------------------------------------------------
 
 import unittest
+from numpy.testing import assert_array_equal
 from gimber.display import *
 
 
@@ -36,6 +37,28 @@ class DisplayTests(unittest.TestCase):
 
         actions = display.actionsFrom(1)
         self.assertEqual(len(actions), 1)
+
+
+    def testLoadImage(self):
+        display = Display()
+        # Create a syntethic random image
+        image = np.array(np.random.rand(3, 4) * 256, dtype=np.uint8)
+        display.loadImage(image)
+        assert_array_equal(display.tileCreator.image, image)
+
+        actions = display.actionsFrom(0)
+        self.assertEqual(len(actions), 1)
+        self.assertIsInstance(actions[0], LoadImage)
+
+        image = np.array(np.random.rand(3, 4) * 256, dtype=np.uint8)
+        display.loadImage(image)
+        assert_array_equal(display.tileCreator.image, image)
+
+        # Now the previous load image action should be an EmptyAction
+        actions = display.actionsFrom(0)
+        self.assertEqual(len(actions), 2)
+        self.assertIsInstance(actions[0], EmptyAction)
+        self.assertIsInstance(actions[1], LoadImage)
 
 
     def testClear(self):

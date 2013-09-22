f8825979fee7fd5b5cd8db6547b4f3933fee4452
Muffo <muffo87@gmail.com>
2013-09-22 19:35:31 +0100

    New Display hierarchy
    - Added: AbstractDisplay class
    - Added: LocalDisplay and RemoteDisplay classes
    - Added: RemoteDisplayManager class
    - Added: ServerSideException error class
    - Added: tests for the new classes
    - Code refactoring

--- gimber/display.py
+++ gimber/display.py
@@ -9,40 +9,24 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
+import requests
+from urlparse import urljoin
+import json
+
 from tiles import *
 from actions import *
 from paths import *
 
-class Display(object):
+class AbstractDisplay(object):
     def __init__(self):
-        self._tileCreator = None
-        self._actions = []
-
-    @property
-    def tileCreator(self):
-        return self._tileCreator
-
-    @property
-    def actions(self):
-        return self._actions
+        raise RuntimeError("Cannot create an instance of AbstractDisplay")
 
     def actionsFrom(self, last):
-        return self._actions[last:]
+        raise NotImplementedError()
 
-    def _replaceActions(self, oldActionType, newActionType):
-        for i, action in enumerate(self._actions):
-            if isinstance(action, oldActionType):
-                self._actions[i] = newActionType()
 
     def do(self, action):
-        if isinstance(action, ClearPaths):
-            self._replaceActions(AddPaths, EmptyAction)
-
-        if isinstance(action, LoadImage):
-            self._replaceActions(LoadImage, EmptyAction)
-            self._tileCreator = TileCreator(action.image)
-
-        self._actions.append(action)
+        raise NotImplementedError()
 
 
     def addMarker(self, point, options={}):
@@ -65,3 +49,148 @@ class Display(object):
     def loadImage(self, image):
         action = LoadImage(image)
         self.do(action)
+
+#-------------------------------------------------------------------------------
+
+
+class LocalDisplay(AbstractDisplay):
+    def __init__(self):
+        self._tileCreator = None
+        self._actions = []
+
+
+    @property
+    def tileCreator(self):
+        return self._tileCreator
+
+
+    @property
+    def actions(self):
+        return self._actions
+
+
+    def actionsFrom(self, last):
+        return self._actions[last:]
+
+
+    def _replaceActions(self, oldActionType, newActionType):
+        for i, action in enumerate(self._actions):
+            if isinstance(action, oldActionType):
+                self._actions[i] = newActionType()
+
+
+    def do(self, action):
+        if isinstance(action, ClearPaths):
+            self._replaceActions(AddPaths, EmptyAction)
+
+        if isinstance(action, LoadImage):
+            self._replaceActions(LoadImage, EmptyAction)
+            self._tileCreator = TileCreator(action.image)
+
+        self._actions.append(action)
+
+
+#-------------------------------------------------------------------------------
+
+
+class ServerSideError(Exception):
+    def __init__(self, message, details=""):
+         self.message = message
+         self.details = details
+
+
+    @classmethod
+    def fromDict(cls, d):
+        if d.has_key('details'):
+            return cls(d['message'], d['details'])
+        else:
+            return cls(d['message'])
+
+
+    def __str__(self):
+         return self.message
+
+
+
+class RemoteDisplay(AbstractDisplay):
+    def __init__(self, name, url):
+        self._name = name
+        self._url = url
+        self._session = requests.Session()
+
+
+    @property
+    def name(self):
+        return self._name
+
+
+    @property
+    def url(self):
+        return self._url
+
+
+    @property
+    def doUrl(self):
+        return urljoin(self._url, "do/" + self.name)
+
+
+    @property
+    def actionsUrl(self):
+        return urljoin(self._url, "actions/" + self.name)
+
+
+    def actionsFrom(self, last):
+        actionsUrl = self.actionsUrl + "/" + str(last)
+        response = self._session.get(actionsUrl).json()
+        if response['result'] != "ok":
+            raise ServerSideError.fromDict(response)
+        return [ActionParser.fromDict(d) for d in response['actions']]
+
+
+    def do(self, action):
+        actionJson = json.dumps(action.dict)
+        response = self._session.post(self.doUrl, data=actionJson).json()
+        if response['result'] != "ok":
+            raise ServerSideError.fromDict(response)
+
+
+
+class RemoteDisplayManager(object):
+    def __init__(self, url):
+        self._url = url
+        self._displays = {}
+        self._session = requests.Session()
+
+
+    @property
+    def url(self):
+        return self._url
+
+
+    @property
+    def createUrl(self):
+        return urljoin(self._url, "create")
+
+
+    @property
+    def deleteUrl(self):
+        return urljoin(self._url, "delete")
+
+
+    def create(self, name):
+        reqJson = json.dumps({"displayId": name})
+        response = self._session.post(self.createUrl, data=reqJson).json()
+        if response['result'] != "ok":
+            raise ServerSideError.fromDict(response)
+        return RemoteDisplay(name, self.url)
+
+
+    def delete(self, name):
+        reqJson = json.dumps({"displayId": name})
+        response = self._session.post(self.deleteUrl, data=reqJson).json()
+        if response['result'] != "ok":
+            raise ServerSideError.fromDict(response)
+
+
+
+
--- test/testDisplay.py
+++ test/testDisplay.py
@@ -14,10 +14,9 @@ from numpy.testing import assert_array_equal
 from gimber.display import *
 
 
-class DisplayTests(unittest.TestCase):
-
-    def testAddPaths(self):
-        display = Display()
+class AbstractDisplayTests(unittest.TestCase):
+    """Contains tests that can be done both for LocalDisplay and RemoteDispla"""
+    def addPaths(self, display):
         actions = display.actionsFrom(0)
         self.assertEqual(len(actions), 0)
 
@@ -38,9 +37,31 @@ class DisplayTests(unittest.TestCase):
         actions = display.actionsFrom(1)
         self.assertEqual(len(actions), 1)
 
+    def clear(self, display):
+        display.addMarker([10, 10])
+        display.addMarker([12, 12])
+        actions = display.actionsFrom(0)
+        self.assertEqual(len(actions), 2)
+        self.assertIsInstance(actions[0], AddPaths)
+        self.assertIsInstance(actions[1], AddPaths)
+
+        display.clear()
+        actions = display.actionsFrom(0)
+        self.assertEqual(len(actions), 3)
+        self.assertIsInstance(actions[0], EmptyAction)
+        self.assertIsInstance(actions[1], EmptyAction)
+        self.assertIsInstance(actions[2], ClearPaths)
+
+
+class LocalDisplayTests(AbstractDisplayTests):
+
+    def testAddPaths(self):
+        display = LocalDisplay()
+        self.addPaths(display)
+
 
     def testLoadImage(self):
-        display = Display()
+        display = LocalDisplay()
         # Create a syntethic random image
         image = np.array(np.random.rand(3, 4) * 256, dtype=np.uint8)
         display.loadImage(image)
@@ -62,24 +83,12 @@ class DisplayTests(unittest.TestCase):
 
 
     def testClear(self):
-        display = Display()
-        display.addMarker([10, 10])
-        display.addMarker([12, 12])
-        actions = display.actionsFrom(0)
-        self.assertEqual(len(actions), 2)
-        self.assertIsInstance(actions[0], AddPaths)
-        self.assertIsInstance(actions[1], AddPaths)
-
-        display.clear()
-        actions = display.actionsFrom(0)
-        self.assertEqual(len(actions), 3)
-        self.assertIsInstance(actions[0], EmptyAction)
-        self.assertIsInstance(actions[1], EmptyAction)
-        self.assertIsInstance(actions[2], ClearPaths)
+        display = LocalDisplay()
+        self.clear(display)
 
 
     def testDict(self):
-        display = Display()
+        display = LocalDisplay()
         display.addMarker([10, 10])
         display.addMarker([12, 12])
         actions = display.actionsFrom(0)
@@ -87,6 +96,39 @@ class DisplayTests(unittest.TestCase):
         self.assertEqual(actions[0].dict['atype'], 'addpaths')
         self.assertEqual(len(actions[0].dict['paths']), 1)
         self.assertEqual(actions[0].dict['paths'][0]['ptype'], 'marker')
+
+
+class RemoteDisplayManagerTests(unittest.TestCase):
+    def testCreateDelete(self):
+        rdm = RemoteDisplayManager("http://localhost:8080")
+
+        displayId1 = "disp1"
+        displayId2 = "disp2"
+
+        rdm.create("disp1")
+        rdm.create("disp2")
+        self.assertRaises(ServerSideError, rdm.create, "disp1")
+        self.assertRaises(ServerSideError, rdm.create, "disp2")
+
+        rdm.delete("disp1")
+        rdm.delete("disp2")
+        self.assertRaises(ServerSideError, rdm.delete, "disp1")
+        self.assertRaises(ServerSideError, rdm.delete, "disp2")
+
+
+class RemoteDisplayTests(AbstractDisplayTests):
+
+    def testAddPaths(self):
+        rdm = RemoteDisplayManager("http://localhost:8080")
+        display = rdm.create("disp1")
+        self.addPaths(display)
+        rdm.delete("disp1")
+
+    def testClear(self):
+        rdm = RemoteDisplayManager("http://localhost:8080")
+        display = rdm.create("disp1")
+        self.clear(display)
+        rdm.delete("disp1")
 
 
 

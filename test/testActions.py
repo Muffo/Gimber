2703ff051453a4b94e1772e5b935364da0b052ce
Muffo <muffo87@gmail.com>
2013-10-02 11:41:35 +0200

    Added test of the property 'dict'

--- test/testActions.py
+++ test/testActions.py
@@ -21,24 +21,29 @@ from gimber.paths import *
 class ActionParsingTests(unittest.TestCase):
 
     def testAddPaths(self):
-        addPathsAction = ActionParser.fromDict({
+        addPathsDict = {
             'atype': 'addpaths',
             'paths': [
                 {
                     'ptype': 'marker',
-                    'point': [10, 12]
+                    'point': [10.0, 12.0],
+                    'options': {}
                 },
                 {
                     'ptype': 'line',
-                    'points': [[10, 12], [13, 15]]
+                    'points': [[10.0, 12.0], [13.0, 15.0]],
+                    'options': {}
                 }
             ]
-        })
+        }
 
+        addPathsAction = ActionParser.fromDict(addPathsDict)
         self.assertIsInstance(addPathsAction, AddPaths)
         self.assertIsInstance(addPathsAction.paths[0], Marker)
         self.assertIsInstance(addPathsAction.paths[1], Line)
 
+        addPathsAction2 = AddPaths([Marker([10, 12]), Line([[10, 12], [13, 15]])])
+        self.assertDictEqual(addPathsDict, addPathsAction2.dict)
 
 
     def testClearPaths(self):
@@ -59,13 +64,19 @@ class ActionParsingTests(unittest.TestCase):
         # Create a syntethic random image
         image = np.array(np.random.rand(3, 4) * 256, dtype=np.uint8)
 
-        loadImageAction = ActionParser.fromDict({
+        loadImageDict = {
              'atype': 'loadimage',
              'image': base64.b64encode(image),
              'shape': image.shape
-        })
+        }
+
+        loadImageAction = ActionParser.fromDict(loadImageDict)
         self.assertIsInstance(loadImageAction, LoadImage)
         assert_array_equal(loadImageAction.image, image)
+
+        loadImageAction2 = LoadImage(image)
+        self.assertDictEqual(loadImageAction2.dict, loadImageDict)
+
 
         self.assertRaises(KeyError, ActionParser.fromDict, {
             'atype': 'loadimage',

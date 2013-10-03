ee5883b543ae194628c44b9ceb039fcc42dd4b9c
Muffo <muffo87@gmail.com>
2013-10-03 15:51:41 +0200

    Improved web viewer
    + Pixel info control: shows the coordinates and the color of the pixel
    under the cursor
    - Fixed coordinates system introducing the zoomOffset
    - Changed default values of the options of the Marker

--- gimber/resources/js/gimber.js
+++ gimber/resources/js/gimber.js
@@ -8,9 +8,11 @@ $(function() {
 
 
 	var tileUrl = '/tile/' + displayId + '/{z}/{x}/{y}'
+	var zoomOffset = -3
+
 	var tiles = L.tileLayer(tileUrl, {
 		attribution: 'Gimber',
-		zoomOffset: -3,
+		zoomOffset: zoomOffset,
 		continuousWorld: true
 	}).addTo(map);
 
@@ -35,50 +37,44 @@ $(function() {
 	*/
 
 
-	var popup = L.popup();
-	function onMapClick(e) {
-		x = e.latlng.lng
-		y = -e.latlng.lat
-
-		popup
-			.setLatLng(e.latlng)
-			.setContent("x=" + x + " y=" + y)
-			.openOn(map);
-	}
-	map.on('click', onMapClick);
-
-
-	function onMapMouseMove(e) {
-		x = e.latlng.lng
-		y = -e.latlng.lat
-		console.log("x=" + x + " y=" + y)
-	}
-	// map.on('mousemove', onMapMouseMove);
-
-
 	// -----------------------------------------------------------------
 
-	function loadImage() {
+	var imageArray = null
+	var imageShape = null
+
+	function loadImage(image, shape) {
+		var raw = window.atob(image)
+		var rawLength = raw.length
+		imageArray = new Uint8Array(new ArrayBuffer(rawLength))
+
+		for(i = 0; i < rawLength; i++) {
+			imageArray[i] = raw.charCodeAt(i)
+		}
+		imageShape = shape
+
 		tiles.redraw()
 	}
 
 
 	function unproject(point) {
-		return [-point[1], point[0]]
+		return [-point[1] * Math.pow(2, zoomOffset), point[0] * Math.pow(2, zoomOffset)]
 	}
+
 
 	function addPaths(paths) {
 		$.each(paths, function(index, path){
 			switch (path.ptype) {
 				case "marker":
+					point = unproject(path.point)
 					if (Object.keys(path.options).length != 0) {
-						var marker = L.circleMarker(unproject(path.point), path.options)
+						var marker = L.circleMarker(point, path.options)
 					} else {
-						var marker = L.circleMarker(unproject(path.point), 
-							{'radius': 2, 'color': 'blue', 'fillColor': 'red'})
+						var marker = L.circleMarker(point, 
+							{'radius': 3, 'color': 'blue', 'fillColor': 'red'})
 					}
+					marker.bindPopup("x=" + path.point[0] + " y=" + path.point[1])
 					overlays.addLayer(marker) 
-					//marker.addTo(map)
+					
 					break
 
 				case "line":
@@ -86,10 +82,9 @@ $(function() {
 						var line = L.circleMarker(unproject(path.points[0]), path.options)
 					} else {
 						var line = L.circleMarker(unproject(path.points[0]), 
-							{'radius': 2, 'color': 'blue', 'fillColor': 'red'})
+							{'radius': 3, 'color': 'blue', 'fillColor': 'red'})
 					}
 					overlays.addLayer(line) 
-					// line.addTo(map)
 					break
 			}
 		})
@@ -104,7 +99,7 @@ $(function() {
 	function dispatch(action) {
 		switch (action.atype) {
 			case "loadimage":
-				loadImage()
+				loadImage(action.image, action.shape)
 				break
 
 			case "addpaths":
@@ -120,6 +115,53 @@ $(function() {
 		}
 	}
 
+	// ------------------------------------------------------------------
+
+	var PixelInfoControl = L.Control.extend({
+	    options: {
+	        position: 'topright'
+	    },
+
+	    onAdd: function (map) {
+	        container = L.DomUtil.create('div', 'pixel-info-control');
+	        container.innerHTML = "<p>Pixel Info Control</p>"
+
+	        function onMapMouseMove(e) {
+		    	x = e.latlng.lng / Math.pow(2, zoomOffset)
+				y = -e.latlng.lat / Math.pow(2, zoomOffset)
+				container.innerHTML = "<p>x=" + x.toFixed(3) + " y=" + y.toFixed(3) + "</p>"
+
+				if (imageArray === null)
+					return
+
+				if (x < 0 || x > imageShape[1] || y < 0 || x > imageShape[0])
+					return 
+
+				if (imageShape.length == 2) {
+					var intensity = imageArray[Math.floor(x) + Math.floor(y) * imageShape[1]]
+					var colorStr = "<p>Intensity: " + intensity
+					colorStr += ' <span style="border: 1px;display: block;background: rgb('+ intensity +', '+ intensity +', '+ intensity +'); float: right;width: 15px;height: 15px;"> </span>'
+					colorStr += "</p>"
+				}
+				else {
+					var colorR = imageArray[3 * Math.floor(x) + 3 * Math.floor(y) * imageShape[1]]
+					var colorG = imageArray[3 * Math.floor(x) + 3 * Math.floor(y) * imageShape[1] + 1]
+					var colorB = imageArray[3 * Math.floor(x) + 3 * Math.floor(y) * imageShape[1] + 2]
+					var colorStr = "<p>Color: (" + colorR + ", " + colorG + ", " + colorB + ")"
+					colorStr += ' <span style="border: 1px;display: block;background: rgb('+ colorR +', '+ colorG +', '+ colorB +'); float: right;width: 15px;height: 15px;"> </span>'
+					colorStr += "</p>"
+				}
+
+				container.innerHTML += colorStr
+		    }
+
+	        map.on('mousemove', onMapMouseMove);
+
+	        return container;
+	    }
+	});
+
+	map.addControl(new PixelInfoControl());
 
 	// -----------------------------------------------------------------
 

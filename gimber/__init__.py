9098ada59faf04d73658e507f018cbfa4993c152
Muffo <muffo87@gmail.com>
2013-12-27 18:09:39 +0100

    Added __init__.py file

---
+++ gimber/__init__.py
@@ -0,0 +1,5 @@
+from display import RemoteDisplayManager, RemoteDisplay
+from actions import AddPaths, ClearPaths, LoadImage
+from paths import Marker, Line
+from opencv import drawChessboardCorners, drawContours
+
